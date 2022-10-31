4f310bd5c2339b2c2c3308133b3d2213734cd062
ryo9354 <ryo9354@gmail.com>
2022-10-31 22:00:45 +0900

    全体を1つの関数に変更

--- reply_only_contents.js
+++ reply_only_contents.js
@@ -1,15 +1,22 @@
-  if(message.text == "ねこ"){
-    msg = "にゃーん";
+// dicebotの成功/失敗に応答するbot
+// 成否判定ロールでなければ投稿のjsonが返る
+
+function homeBot(message){
+    msg = null
+    if(message.text == "ねこ"){
+      msg = "にゃーん";
+      }
+    else if(message.extend?.roll?.critical){
+      msg = + message.extend?.roll?.dices[0].value + "クリ？えらすぎる……"; 
     }
-  else if(message.extend?.roll?.critical){
-    msg = + message.extend?.roll?.dices[0].value + "クリ？えらすぎる……"; 
-  }
-  else if(message.extend?.roll?.fumble){
-    msg = "……ってことは次は成功するから！"; 
-  }
-  else if(message.extend?.roll?.success){
-    msg = message.extend?.roll?.dices[0].value + "成功！さすが！"; 
-  }
-  else if(message.extend?.roll?.failure){
-    msg = message.extend?.roll?.dices[0].value + "か、惜しかったね……";
+    else if(message.extend?.roll?.fumble){
+      msg = "……ってことは次は成功するから！"; 
+    }
+    else if(message.extend?.roll?.success){
+      msg = message.extend?.roll?.dices[0].value + "成功！さすが！"; 
+    }
+    else if(message.extend?.roll?.failure){
+      msg = message.extend?.roll?.dices[0].value + "か、惜しかったね……";
+    }
+    return msg
   }
